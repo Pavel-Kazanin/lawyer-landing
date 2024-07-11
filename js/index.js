f69192cea3bb5cbba8a36617110684f99ad4d559
Pavel Kazanin <pkazanin@yandex.ru>
2024-07-11 18:22:48 +0300

    lead section with slider adaptive

--- js/index.js
+++ js/index.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
   $('.lead__slider').slick({
-    appendArrows:$('.lead-slider__control-panel')
+    dots: true,
+    arrows: true,
+    appendArrows:$('.control-panel__arrows'),
+    appendDots:$('.control-panel__dots'),
+    draggable: false
   });
+});
+
+const burgerOpenButton = document.querySelector('.menu__burger-icon');
+const burgerCloseButton = document.querySelector('.menu__close-button');
+const burgerMenu = document.querySelector('.menu__list');
+
+burgerOpenButton.addEventListener('click', function (event) {
+  burgerMenu.classList.add('menu__list-active');
+});
+
+burgerCloseButton.addEventListener('click', function (event) {
+  burgerMenu.classList.remove('menu__list-active');
 });
