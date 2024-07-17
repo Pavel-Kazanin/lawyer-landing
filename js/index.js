442470a1698e33937f720fc79008938281946037
Pavel Kazanin <pkazanin@yandex.ru>
2024-07-17 23:28:56 +0300

    services section adaptive with slider process

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Инициализация слайдера lead секции
 $(document).ready(function(){
   $('.lead__slider').slick({
     dots: true,
@@ -8,14 +9,58 @@ $(document).ready(function(){
   });
 });
 
+$(document).ready(function(){
+  console.log("слайдер добавлен");
+  $('.services__slider').slick({  
+    dots: false,
+    arrows: false,
+    initialSlide: 0,
+    infinite: false,
+    slidesToShow: 1,
+    focusOnSelect: true       
+  });
+  someFunc();
+});
+
+// Включение и отключение services слайдера, при изменении ширины экрана
+function someFunc() {  
+  var w = window.innerWidth;
+  console.log("dfdjdfj");
+  if (w > 560) {
+    console.log("22222");
+    $('.services__slider').slick('unslick');
+  } else {
+    $(document).ready(function(){
+      $('.services__slider').slick({  
+        dots: false,
+        arrows: false,
+        mobileFirst: true,
+        slidesToShow: 2,
+        swipeToSlide: true           
+      });
+    });
+  }
+  console.log("SomeFunc сработал");
+}
+
+// someFunc();
+
+window.addEventListener("resize", function() {
+  console.log("Resize сработал");
+  someFunc();  
+});
+
+// Элементы бургер меню
 const burgerOpenButton = document.querySelector('.menu__burger-icon');
 const burgerCloseButton = document.querySelector('.menu__close-button');
 const burgerMenu = document.querySelector('.menu__list');
 
-burgerOpenButton.addEventListener('click', function (event) {
-  burgerMenu.classList.add('menu__list-active');
-});
+// Слушатель клика на открытие бургер меню
+// burgerOpenButton.addEventListener('click', function (event) {
+//   burgerMenu.classList.add('menu__list-active');
+// });
 
-burgerCloseButton.addEventListener('click', function (event) {
-  burgerMenu.classList.remove('menu__list-active');
-});
+// // Слушатель клика на закрытие бургер меню
+// burgerCloseButton.addEventListener('click', function (event) {
+//   burgerMenu.classList.remove('menu__list-active');
+// });
